77dfef97f827341e793b66a7ca69376b5d11f61a
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2025-06-08 20:58:19 -0300

    first endpoint

---
+++ src/test/java/com/pedroacbg/rest_with_spring_boot/RestWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroacbg.rest_with_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
