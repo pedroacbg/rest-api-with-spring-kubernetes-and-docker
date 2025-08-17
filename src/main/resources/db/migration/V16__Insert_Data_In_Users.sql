3b35c44d0c115dec0799dd72c97dced553a9ade2
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2025-08-17 17:23:55 -0300

    implementing SecurityConfig

--- src/main/resources/db/migration/V16__Insert_Data_In_Users.sql
+++ src/main/resources/db/migration/V16__Insert_Data_In_Users.sql
@@ -1,3 +1,3 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-	('jeraldo', 'Jeraldo Cleber', '1e3cdeeaaaeeda173ff6d002e7cb5e3f91ebc354dcff52156c9eaba1793a3a5e5bee306c11099e22', b'1', b'1', b'1', b'1'),
-	('mansone', 'Mansone Espirgo', '362ad02420268beeb22d3a1f0d92749df461d7f4b74c9433d7415bdeef1b2902f4eb1edaecb37cb3', b'1', b'1', b'1', b'1');
+	('jeraldo', 'Jeraldo Cleber', '14ee31539425c878709abdf839f4310d2cfbdc5483f398d715109a1c1074c8e2bd4d18c1df0b1f97', b'1', b'1', b'1', b'1'),
+	('mansone', 'Mansone Espirgo', 'e482e903c7d67f597a4dca33cdc80115ad166766387aee4a1c42c90855a0dbd63668e58648e71397', b'1', b'1', b'1', b'1');
